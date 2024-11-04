14614e2fcc05d11f9178905ab45325773a83a222
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-04 21:47:43 +0100

    feat: added custom error boundary setup
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React, { ErrorInfo, ReactNode } from "react";
+import Flytrap from "./index"; 
+
+interface ErrorBoundaryProps {
+  flytrap: Flytrap,
+  children: ReactNode,
+  fallback?: ReactNode,
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  private flytrap: Flytrap;
+
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+    this.flytrap = props.flytrap;
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
