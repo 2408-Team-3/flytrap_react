9a047d56bd3545b63607d7119364bbc18f1d2c21
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-08 18:28:15 +0100

    feat: added custom FlytrapError and added ErrorBoundary as a static property
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import sourceMapSupport from 'source-map-support';
 import { LogData, RejectionValue } from "./types/types";
 import { responseSchema } from "./types/schemas";
+import { FlytrapError } from "./utils/FlytrapError";
 import { ZodError } from "zod";
+import { ErrorBoundary } from './ErrorBoundary';
 
-export default class Flytrap {
+class Flytrap {
   private projectId: string;
   private apiEndpoint: string;
   private apiKey: string;
@@ -42,6 +44,7 @@ export default class Flytrap {
 
   private handleUncaughtException(e: ErrorEvent): void {
     if (e.error) {
+      if (e.error instanceof FlytrapError) return;
       this.logError(e.error, false);
     }
   }
@@ -50,9 +53,45 @@ export default class Flytrap {
     const { reason } = e;
 
     if (reason instanceof Error) {
+      if (reason instanceof FlytrapError) return;
       this.logError(reason, false);
     } else {
       this.logRejection(reason, false);
+    }
+  }
+
+  private async logError(error: Error, handled: boolean): Promise<void> {
+    const data: LogData = {
+      error: {
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      },
+      handled,
+      timestamp: new Date().toISOString(),
+      project_id: this.projectId,
+    };
+
+    try {
+      console.log("[flytrap] Sending error to backend...");
+      const response = await axios.post(
+        `${this.apiEndpoint}/api/errors`,
+        { data },
+        { headers: { "x-api-key": this.apiKey } },
+      );
+
+      responseSchema.parse(response);
+      console.log("[flytrap]", response.status, response.data);
+    } catch (e) {
+      if (e instanceof ZodError) {
+        console.error("[flytrap] Response validation error:", e.errors);
+      } else {
+        console.error("[flytrap] An error occurred sending error data:", e);
+        throw new FlytrapError(
+          "An error occurred logging error data.",
+          e instanceof Error ? e : new Error(String(e)),
+        );
+      }
     }
   }
 
@@ -89,33 +128,9 @@ export default class Flytrap {
     }
   }
 
-  private async logError(error: Error, handled: boolean): Promise<void> {
-    const data: LogData = {
-      error: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-      handled,
-      timestamp: new Date().toISOString(),
-      project_id: this.projectId,
-    };
-
-    try {
-      const response = await axios.post(
-        `${this.apiEndpoint}/api/errors`,
-        { data },
-        { headers: { "x-api-key": this.apiKey } },
-      );
-      responseSchema.parse(response);
-      console.log("[flytrap]", response.status, response.data);
-    } catch (e) {
-      if (e instanceof ZodError) {
-        console.error("[flytrap] Response validation error:", e.errors);
-      } else {
-        console.error("[flytrap] An error occurred sending error data:", e);
-        throw new Error("An error occurred logging error data.");
-      }
-    }
-  }
+  static ErrorBoundary: typeof ErrorBoundary;
 }
+
+Flytrap.ErrorBoundary = ErrorBoundary;
+
+export default Flytrap;
