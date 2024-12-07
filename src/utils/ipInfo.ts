a115884605af77f30e0448d2c84caaf202759be2
Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
2024-12-07 18:15:52 -0500

    fix: add return type to getIpAddress function
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/utils/ipInfo.ts
+++ src/utils/ipInfo.ts
@@ -4,9 +4,9 @@ import axios from 'axios';
  * Retrieves the public IP address of the current environment using the ipify API.
  * @returns A promise resolving to the IP address as a string, or `null` if the request fails.
  */
-export const getIpAddress = async () => {
+export const getIpAddress = async (): Promise<string | null> => {
   try {
-    const { data }= await axios.get('https://api.ipify.org?format=json');
+    const { data } = await axios.get('https://api.ipify.org?format=json');
     return data.ip;
   } catch {
     return null;
